50c2969add132d1d27bc97952fabc25aef79b5ed
Pis4ni <pis4wrk@gmail.com>
2023-09-26 10:27:08 +0200

    add: corrected version

--- censor.php
+++ censor.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 
-$txtToCensor =  trim($_GET["txtToCensor"]);
+$txtToCensor = trim($_GET["txtToCensor"]);
 $wordToCensor = trim($_GET["wordToCensor"]);
-
+$txtLength = strlen($txtToCensor);
 $censored = str_replace($wordToCensor, '<***>', $txtToCensor);
+$censorLength = strlen($censored);
 
 
 ?>
@@ -11,29 +12,43 @@ $censored = str_replace($wordToCensor, '<***>', $txtToCensor);
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="./node_modules/bootstrap/dist/css/bootstrap.min.css"
-    />
-    <link rel="stylesheet" href="./css/style.css" />
-    <title>Document</title>
-  </head>
-  <body>
-    <main class="d-flex align-items-center justify-content-center">
-        <div class="card w-50">
-            <div class="card-body">
-                <h5 class="card-title">Censored version of your Text</h5>
-                
-                <p class="card-text"><?php echo $censored ?></p>
 
-                <a href="./" class="btn btn-primary">Back to Home</a>
-            </div>
-        </div>
-    </main>
-  </body>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <title>Document</title>
+</head>
+
+<body>
+  <main class="d-flex align-items-center justify-content-center flex-column">
+    <div class="card w-50 mb-5">
+      <div class="card-body">
+        <h5 class="card-title">Your Text</h5>
+
+        <p class="card-text">
+          <?php echo $txtToCensor ?>
+        </p>
+        <span class="card-subtitle">
+          your text is long: <?php echo $txtLength ?>
+        </span>
+      </div>
+    </div>
+    <div class="card w-50">
+      <div class="card-body">
+        <h5 class="card-title">Censored version of your Text</h5>
+
+        <p class="card-text">
+          <?php echo $censored ?>
+        </p>
+        <span class="card-subtitle">
+          your text is now long: <?php echo $censorLength ?>
+        </span>
+      </div>
+    </div>
+    <a href="./" class="btn btn-outline-success my-3">Back to Home</a>
+  </main>
+</body>
+
 </html>
-
-
